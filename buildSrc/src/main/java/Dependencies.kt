5e8ff3ef77338a6b582d910703befdf433cb631b
admin <450121066@qq.com>
2020-06-06 10:45:46 +0800

    add usecase layer

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,6 +1,22 @@
+import Versions.kotlinCoroutineAndroidVersion
+import Versions.kotlinCoroutineCoreVersion
+
 object Versions {
     val kotlin = "1.3.72"
+    val kotlinCoroutineCoreVersion="1.3.7"
+    val kotlinCoroutineAndroidVersion="1.3.5"
+
+    val naviagtionVersion="2.3.0-alpha01"
 }
+
+
+
+
 object Dependencies {
+
     val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+
+    val kotlinCoroutineCore="org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutineCoreVersion"
+
+    val kotlinCoroutineAndroid="org.jetbrains.kotlinx:kotlinx-coroutines-android:${kotlinCoroutineAndroidVersion}"
 }
---
+++ usecase-core/src/main/java/com/jsn/usecase_core/UseCases.kt
@@ -0,0 +1,46 @@
+package com.jsn.usecase_core
+
+import android.view.animation.AnticipateInterpolator
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.flow.*
+
+interface ObservableUseCase<T>{
+
+    val dispatcher:CoroutineDispatcher
+
+    fun observe():Flow<T>
+
+}
+
+@OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+abstract class FlowUseCase<T:Any,params:Any>:ObservableUseCase<T>{
+
+    private val channel=ConflatedBroadcastChannel<params>()
+
+    protected abstract fun doWork(params: params):Flow<T>
+
+    operator fun invoke(params: params) =channel.sendBlocking(params)
+
+    fun produce(params: params)=doWork(params).flowOn(dispatcher)
+
+    override fun observe(): Flow<T> = channel.asFlow()
+        .distinctUntilChanged()
+        .flatMapConcat { params ->
+            doWork(params)
+        }
+        .flowOn(dispatcher)
+}
+
+abstract class NoResultUseCase<in T>{
+
+    protected abstract val dispatcher:CoroutineDispatcher
+
+    suspend operator fun invoke(params:T)= withContext(dispatcher){
+        run(params)
+    }
+
+    abstract suspend fun run(params: T)
+
+}
